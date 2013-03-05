9252e6f00e737690547937684fa6604b6de51a02
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2013-03-05 14:18:58 +0400

    updated for Scala 2.10 + cleanup

--- README-scala.txt
+++
@@ -1,1 +0,0 @@
-Please see http://bitbucket.org/dickwall/scala-koans/wiki/Home for more details on scala-koans
--- src/test/scala/org/functionalkoans/forscala/support/KoanSuite.scala
+++ src/test/scala/org/functionalkoans/forscala/support/KoanSuite.scala
@@ -1,61 +1,59 @@
 package org.functionalkoans.forscala.support
 
-import org.scalatest.Reporter
-import org.scalatest.Stopper
-import org.scalatest.Distributor
-import org.scalatest.Filter
-import org.scalatest.Tracker
-import org.scalatest.FunSuite
+import org.scalatest._
 import org.scalatest.events.Event
 import org.scalatest.events.TestSucceeded
+import scala.Some
 
 trait KoanSuite extends FunSuite {
-  override def runTests(testName: Option[String], reporter: Reporter, stopper: Stopper, filter: Filter,
-                        configMap: Map[String, Any], distributor: Option[Distributor], tracker: Tracker) {
+  protected override def runTests(testName: Option[String], args: Args): Status = {
+    args match {
+      case Args(reporter, stopper, filter, configMap, distributor, tracker, _, _, _, _) =>
+        if (testName == null)
+          throw new NullPointerException("testName was null")
+        if (reporter == null)
+          throw new NullPointerException("reporter was null")
+        if (stopper == null)
+          throw new NullPointerException("stopper was null")
+        if (filter == null)
+          throw new NullPointerException("filter was null")
+        if (configMap == null)
+          throw new NullPointerException("configMap was null")
+        if (distributor == null)
+          throw new NullPointerException("distributor was null")
+        if (tracker == null)
+          throw new NullPointerException("tracker was null")
 
-    if (testName == null)
-      throw new NullPointerException("testName was null")
-    if (reporter == null)
-      throw new NullPointerException("reporter was null")
-    if (stopper == null)
-      throw new NullPointerException("stopper was null")
-    if (filter == null)
-      throw new NullPointerException("filter was null")
-    if (configMap == null)
-      throw new NullPointerException("configMap was null")
-    if (distributor == null)
-      throw new NullPointerException("distributor was null")
-    if (tracker == null)
-      throw new NullPointerException("tracker was null")
+        class KoanReporter(wrappedReporter: Reporter) extends Reporter {
+          var succeeded = false
 
-    class KoanReporter(wrappedReporter: Reporter) extends Reporter {
-      var succeeded = false
-
-      override def apply(event: Event) = {
-        event match {
-          case e: TestSucceeded => succeeded = true
-          case _ =>
-        }
-        wrappedReporter(event)
-      }
-    }
-
-    val stopRequested = stopper
-
-    // If a testName is passed to run, just run that, else run the tests returned
-    // by testNames.
-    testName match {
-      case Some(tn) => runTest(tn, reporter, stopRequested, configMap, tracker)
-      case None =>
-        val tests = testNames.iterator
-        var failed = false
-        for (test <- tests) {
-          if (failed == false) {
-            val koanReporter = new KoanReporter(reporter)
-            runTest(test, koanReporter, stopper, configMap, tracker)
-            failed = !koanReporter.succeeded
+          override def apply(event: Event) = {
+            event match {
+              case e: TestSucceeded => succeeded = true
+              case _ =>
+            }
+            wrappedReporter(event)
           }
         }
+
+        // If a testName is passed to run, just run that, else run the tests returned
+        // by testNames.
+        testName match {
+          case Some(tn) => runTest(tn, new Args(reporter, stopper, filter, configMap, tracker = tracker))
+          case None =>
+            val tests = testNames.iterator
+            var failed = false
+            for (test <- tests) {
+              if (failed == false) {
+                val koanReporter = new KoanReporter(reporter)
+                runTest(test, new Args(koanReporter, stopper, filter, configMap, tracker = tracker))
+                failed = !koanReporter.succeeded
+              }
+            }
+            if (failed) FailedStatus
+            else SucceededStatus
+        }
+      case _ => SucceededStatus
     }
   }
 
