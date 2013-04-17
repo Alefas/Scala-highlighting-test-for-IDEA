b9c9f421282c62f5313996e368b6c09ebd69b74d
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2013-04-17 20:18:50 +0400

    Test for SCL-5566

---
+++ src/main/java/scl5566/JavaPart.java
@@ -0,0 +1,11 @@
+package scl5566;
+
+import scl5562.ScalaPart;
+import scl5562.ScalaPart1;
+
+public class JavaPart {
+    public static void main(String[] args) {
+        ScalaPart.Z z = new ScalaPart.Z() {};
+        ScalaPart1.Z z1 = new ScalaPart1.Z() {};
+    }
+}
---
+++ src/main/scala/scl5562/ScalaPart.scala
@@ -0,0 +1,17 @@
+package scl5562
+
+trait ScalaPart {
+
+}
+
+object ScalaPart {
+  trait Z
+}
+
+class ScalaPart1 {
+
+}
+
+object ScalaPart1 {
+  trait Z
+}
