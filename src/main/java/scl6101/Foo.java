2b6baf63c50d8a0fca4ddd7e5c5f1c449f2a6f40
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2014-09-24 15:02:53 +0400

    Test for SCL-6101

---
+++ src/main/java/scl6101/Foo.java
@@ -0,0 +1,11 @@
+package scl6101;
+
+/**
+ * @author Alefas
+ * @since 24/09/14.
+ */
+public class Foo {
+    public static void main(String[] args) {
+        Baz.bar_$eq(2); //wrong error highlighting
+    }
+}
---
+++ src/main/scala/scl6101/Baz.scala
@@ -0,0 +1,9 @@
+package scl6101
+
+/**
+ * @author Alefas
+ * @since 24/09/14.
+ */
+object Baz {
+  var bar = 0
+}
