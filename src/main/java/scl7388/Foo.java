42dcc25bb2d64a4b6a32078213423e055c1ff54e
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2014-10-03 14:28:48 +0400

    Test for SCL-7388

---
+++ src/main/java/scl7388/Foo.java
@@ -0,0 +1,9 @@
+package scl7388;
+
+/**
+ * @author Alefas
+ * @since 03/10/14.
+ */
+public class Foo<T> {
+    public Foo(Class<T> clazz, String... labels) {}
+}
---
+++ src/main/scala/scl7388/Goo.scala
@@ -0,0 +1,10 @@
+package scl7388
+
+/**
+ * @author Alefas
+ * @since 03/10/14.
+ */
+class Goo {
+  val z = new Foo(classOf[String], "")
+  val f: Foo[String] = z
+}
